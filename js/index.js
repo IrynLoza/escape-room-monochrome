90955a0acf998263b1913ca106386108deabb7e2
Iryna Loza <irynloza@gmail.com>
2023-04-05 20:48:06 -0700

    Add plant

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 const state = {};
+
+// const openAndClose = (openObject, movingObject) => {   
+//   console.log(`openObject===> ${openObject.src}`) 
+//   if (openObject.src.match("closed")) {
+//     openObject.src = "images/openned-fireplace-door.png";
+//     console.log(`openObject===> ${movingObject.src}`) 
+//     movingObject.style.opacity = state.fire ? 0 : 1;
+//   } else {
+//     openObject.src = "images/closed-fireplace-door.png";
+//     movingObject.style.opacity = 0
+//   }
+// };
